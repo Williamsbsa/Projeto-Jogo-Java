b780915a5898d7697547ec2194097ea717a51400
Williams Braga <bragawilliams@yahoo.com.br>
2018-11-13 01:00:57 -0200

    Teste mudanças

--- ProjetoJogo/src/projetojogo/ProjetoJogo.java
+++ ProjetoJogo/src/projetojogo/ProjetoJogo.java
@@ -127,9 +127,8 @@ public class ProjetoJogo {
         escudoTotal = escudoClasse + escudoItem;
         System.out.println("Dano Total = "+danoTotal);
         System.out.println("Escudo total = "+escudoTotal);
-
         
-        
+        System.out.println("EAE PEDRO");
         
         
         /** cadastrando e listando itens
