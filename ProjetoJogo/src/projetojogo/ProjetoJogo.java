ac7e96f93deaf5e61952b52839f1b684c4decfb7
Pedro <pedro.pradi@pucpr.edu.br>
2018-11-13 01:26:53 -0200

    cao
    mudei

--- ProjetoJogo/src/projetojogo/ProjetoJogo.java
+++ ProjetoJogo/src/projetojogo/ProjetoJogo.java
@@ -127,7 +127,7 @@ public class ProjetoJogo {
         escudoTotal = escudoClasse + escudoItem;
         System.out.println("Dano Total = "+danoTotal);
         System.out.println("Escudo total = "+escudoTotal);
-        //CACHORRO dos inferno
+        //CACHORRO dos inferno lindo e mara
         
         
         
